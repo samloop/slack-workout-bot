17b005f134555aa68e8b62d1fd6559b097895c87
Sam Loop <samloop16@gmail.com>
2019-07-13 17:40:03 -0700

    restarting with botkit

---
+++ src/bot.js
@@ -0,0 +1,49 @@
+//  __   __  ___        ___
+// |__) /  \  |  |__/ |  |
+// |__) \__/  |  |  \ |  |
+
+// This is the main file for the starter-slack bot.
+
+// Import Botkit's core features
+const { Botkit } = require('botkit');
+const { BotkitCMSHelper } = require('botkit-plugin-cms');
+
+// Import a platform-specific adapter for slack.
+
+const { SlackAdapter, SlackMessageTypeMiddleware, SlackEventMiddleware } = require('botbuilder-adapter-slack');
+
+const { MongoDbStorage } = require('botbuilder-storage-mongodb');
+
+// Load process.env values from .env file
+require('dotenv').config();
+
+
+// TO DO: make this the postgres database from Heroku
+let storage = null;
+/*if (process.env.MONGO_URI) {
+    storage = mongoStorage = new MongoDbStorage({
+        url : process.env.MONGO_URI,
+    });
+}*/
+
+
+
+const adapter = new SlackAdapter({
+    // parameters used to secure webhook endpoint
+    verificationToken: process.env.verificationToken,
+    clientSigningSecret: process.env.clientSigningSecret,
+
+    // auth token for a single-team app
+    botToken: process.env.botToken,
+
+    // credentials used to set up oauth for multi-team apps
+    clientId: process.env.clientId,
+    clientSecret: process.env.clientSecret,
+    scopes: ['bot'],
+    redirectUri: process.env.redirectUri,
+
+    // functions required for retrieving team-specific info
+    // for use in multi-team apps
+    getTokenForTeam: getTokenForTeam,
+    getBotUserByTeam: getBotUserByTeam,
+});
